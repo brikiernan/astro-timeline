1edd069c7c3825033524b188ef2202994ac770ba
Brian <brian@briankiernan.com>
2022-09-30 14:02:40 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## Astro UXDS Timeline Testing
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { RuxTimeline } from '@astrouxds/react';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Hey now timeline!</h1>
+      {/* I know I have to do more to impliment :-) */}
+      <RuxTimeline />
+    </div>
+  );
+};
+
+export default App;
